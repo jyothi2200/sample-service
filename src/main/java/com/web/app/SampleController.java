4e7c1c3d9c863e60c5300b7f48549493949ee945
jyothi2466 <settij33@gmail.com>
2022-06-14 18:59:46 +0530

    Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/com/web/app/SampleController.java
@@ -0,0 +1,16 @@
+package com.web.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class SampleController {
+
+	@GetMapping("/say")
+	public String sayhello() {
+		return "Welcome to GITHub";
+
+	}
+}
